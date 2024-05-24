ba7df8842e591ad4c3578299cc51d86e81b2e8f1
saidatulbakdia25 <saidatulbakdia175@gmail.com>
2024-05-24 13:52:55 +0700

    isi artikel blog

--- resources/views/user/blog/index.blade.php
+++ resources/views/user/blog/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('main-content')
 <div class="card mb-3">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="" class="card-img-top" alt="gambar">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <a href="link-target">Selengkapnya</a>
+    <h5 class="card-title">Restoran sushi terbaik</h5>
+    <p class="card-text">Restoran sushi kami, masuk kedalam kategori restoran sushi terbaik yang ada di tokyo</p>
+    <button type="button" onclick="window.location.href='https://www.byfood.com/blog/tokyo/best-sushi-in-tokyo'">Selengkapnya</button>
   </div>
 </div>
 
@@ -15,7 +15,7 @@
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <a href="link-target">Selengkapnya</a>
+      <button type="button" onclick="window.location.href='link-target'">Selengkapnya</button>
     </div>
   </div>
 
@@ -24,7 +24,7 @@
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <a href="link-target">Selengkapnya</a>
+       <button type="button" onclick="window.location.href='link-target'">Selengkapnya</button>
     </div>
   </div>
 
@@ -33,7 +33,7 @@
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <a href="link-target">Selengkapnya</a>
+       <button type="button" onclick="window.location.href='link-target'">Selengkapnya</button>
     </div>
   </div>
   
