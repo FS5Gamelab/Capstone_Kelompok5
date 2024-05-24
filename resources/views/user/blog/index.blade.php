9327d16efd03ec963c65636de54fd318a472ea8c
saidatulbakdia25 <saidatulbakdia175@gmail.com>
2024-05-24 13:54:04 +0700

    isi artikel blog

--- resources/views/user/blog/index.blade.php
+++ resources/views/user/blog/index.blade.php
@@ -13,29 +13,10 @@
 <div class="card mb-3">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <button type="button" onclick="window.location.href='link-target'">Selengkapnya</button>
+      <h5 class="card-title">masuk restoran 10 terbaik</h5>
+      <p class="card-text">restoran kami juga pernah mendapatkan penghargaan sebagai salah satu restoran terbaik menurut data michelin guide tahun 2021</p>
+      <button type="button" onclick="window.location.href='https://www.kompas.com/food/read/2021/12/26/194619575/10-restoran-masakan-indonesia-terbaik-di-dunia-menurut-michelin-guide'">Selengkapnya</button>
     </div>
   </div>
-
-  <div class="card mb-3">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-       <button type="button" onclick="window.location.href='link-target'">Selengkapnya</button>
-    </div>
-  </div>
-
-  <div class="card mb-3">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-       <button type="button" onclick="window.location.href='link-target'">Selengkapnya</button>
-    </div>
-  </div>
-  
 
 @endsection
