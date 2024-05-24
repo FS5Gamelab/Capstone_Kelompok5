e848fd7dfdb10a081ca8eb3d52b138121f80c06a
saidatulbakdia25 <saidatulbakdia175@gmail.com>
2024-05-24 13:36:04 +0700

    blog user statis

--- resources/views/user/blog/index.blade.php
+++ resources/views/user/blog/index.blade.php
@@ -1,4 +1,41 @@
 @extends('layouts.app-user', ['title' => 'Blog'])
 
 @section('main-content')
+<div class="card mb-3">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <a href="link-target">Selengkapnya</a>
+  </div>
+</div>
+
+<div class="card mb-3">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <a href="link-target">Selengkapnya</a>
+    </div>
+  </div>
+
+  <div class="card mb-3">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <a href="link-target">Selengkapnya</a>
+    </div>
+  </div>
+
+  <div class="card mb-3">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <a href="link-target">Selengkapnya</a>
+    </div>
+  </div>
+  
+
 @endsection
